8854b9f05e81cb422b9bc67ea4ebad3ec040eb08
basahota <techbasanta757@gmail.com>
2018-11-16 11:35:28 +0530

    why

--- git-from-eclipse/src/main/java/com/javatechie/git_from_eclipse/App.java
+++ git-from-eclipse/src/main/java/com/javatechie/git_from_eclipse/App.java
@@ -6,7 +6,7 @@ package com.javatechie.git_from_eclipse;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("I love it");
+		System.out.println("I love it");//this is noyt reflected
 		System.out.println("Hello World!" + "This is my first application to acess github from my eclipse");
 	}
 }
