f685e27e81108090a8d205a0d9a94a2abb6894c4
basahota <techbasanta757@gmail.com>
2018-11-16 11:31:29 +0530

    second commit

--- git-from-eclipse/src/main/java/com/javatechie/git_from_eclipse/App.java
+++ git-from-eclipse/src/main/java/com/javatechie/git_from_eclipse/App.java
@@ -4,10 +4,8 @@ package com.javatechie.git_from_eclipse;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!" + "This is my first application to acess github from my eclipse");
+	}
 }
